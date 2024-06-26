8f31697a6ceb09d3e66b441d31bef292fc6ef1ee
nicholas <aigberuan6@gmail.com>
2024-06-26 02:59:25 -0700

    serve

--- index.js
+++ index.js
@@ -11,38 +11,36 @@ app.use(express.json());
 mongoose.connect('mongodb://')
 
 
-app.post('/api/register',async (req, res) => {
+app.post('/api/register', async (req, res) => {
     console.log(req.body);
     try {
-        const user = await User.create({
+        await User.create({
             name: req.body.name,
             email: req.body.email,
-            password:  req.body.password,
+            password: req.body.password,
         })
         res.json({ status: 'ok' }); // This sends the JSON response correctly
     } catch (error) {
-        
+        console.log(error)
+        res.json({ status: 'error', error: 'Duplicates are found' }); // This sends the JSON response correctly    
     }
-    res.json({ status: 'error', error: 'Duplicates are found' }); // This sends the JSON response correctly
+
 });
 
 
-app.post('/api/login',async (req, res) => {
+app.post('/api/login', async (req, res) => {
 
-        const user = await User.findOne({
-            email: req.body.email,
-            password:  req.body.password,
-        })
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
 
-        if (user) {
-            res.json({ status: 'ok' }); // This sends the JSON response correctly
-        } else {
-            res.json({ status: 'error', error: 'User not found' }); // This sends the JSON response correctly
-        }
+    if (user) {
         res.json({ status: 'ok' }); // This sends the JSON response correctly
-    
-        
-    },
- )
+    } else {
+        res.json({ status: 'error', error: 'User not found' }); // This sends the JSON response correctly
+    }
+})
+
 
 app.listen(5000, () => console.log(`Example app listening on port 5000`));
