1dc4e8ce4076a7c5990f91ffdc902053cf16bee8
nicholas <aigberuan6@gmail.com>
2024-07-29 20:47:57 +0100

    backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const User = require('./models/userModels')
-const jwt = require ('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 app.use(cors());
 app.use(express.json());
@@ -30,19 +30,35 @@ app.post('/api/register', async (req, res) => {
 
 app.get('/api/quote', async (req, res) => {
 
-    const token  = req.headers['x-access-token']
-    try{
-    const decoded = jwt.verify(token,'secret123')
-    const email = decoded.email
-    const user = await User.findOne({email:email})
+    const token = req.headers['x-access-token']
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+        const email = decoded.email
+        const user = await User.findOne({ email: email })
 
-        return {status:'ok', quote:user.quote}
-    }catch(error){
+        return { status: 'ok', quote: user.quote }
+    } catch (error) {
         console.log(error)
-        res.json({status:'error', error:'invalid token'})
-    }    
-     })
+        res.json({ status: 'error', error: 'invalid token' })
+    }
+})
 
-    
+
+app.post('/api/quote', async (req, res) => {
+
+    const token = req.headers['x-access-token']
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+        const email = decoded.email
+        await User.updateOne({ email: email },
+            { $set: { quote: req.body.quote } }
+        )
+
+        return { status: 'ok',}
+    } catch (error) {
+        console.log(error)
+        res.json({ status: 'error', error: 'invalid token' })
+    }
+})
 
 app.listen(1337, () => console.log(`Example app listening on port 1337`));
